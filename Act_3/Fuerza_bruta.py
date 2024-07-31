7b76f640ba355ceeabe0c63fc7dd36b12c795e51
Natalia <90008571+StudentNPD@users.noreply.github.com>
2024-07-31 19:09:08 -0400

    Delete Act_3/Fuerza_bruta.py

--- Act_3/Fuerza_bruta.py
+++
@@ -1,6 +0,0 @@
-# Actividad 3 - Fuerza bruta
-# Determinar cuántos intentos son necesarios para encontrar combinaciones numéricas en
-# minúscula. El programa fuerza_bruta.py debe intentar todas las combinaciones de letras
-# posibles, en orden alfabético, hasta que la combinación de letras sea igual a la de la
-# contraseña indicada. Deberá hacer este proceso letra por letra, de izquierda a derecha
-# Probando...
